fa676cb4311f290dd61d08e16c4c661eb4d7edf5
Liam Dawson <liam@ldaws.com>
2021-11-30 00:07:49 +1100

    Ensure logo doesn't hide controls

--- lib/home.dart
+++ lib/home.dart
@@ -157,9 +157,11 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Padding(
-                padding: EdgeInsets.only(left: 100, right: 100),
-                child: Image(image: AssetImage('assets/logo.png'))),
+            Expanded(
+              child: Padding(
+                  padding: EdgeInsets.only(left: 100, right: 100),
+                  child: Image(image: AssetImage('assets/logo.png'))),
+            ),
             Padding(
               padding: EdgeInsets.only(top: 60),
               child: Text(
